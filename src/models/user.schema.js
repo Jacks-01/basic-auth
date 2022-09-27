537320d78d4b4ee73ec718100a21e8115db2e62b
Jack Stubblefield <Jack011601@gmail.com>
2022-09-27 10:42:42 -0500

    halfway through lab

---
+++ src/models/user.schema.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = sequelize.define('User', {
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
+});
